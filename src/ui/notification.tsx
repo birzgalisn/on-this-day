42ef525c28cff64d88f14cdda44113cee187be3d
Niks <hello@niksbirzgalis.com>
2025-03-29 18:56:53 +0000

    feat: add notification banner

---
+++ src/ui/notification.tsx
@@ -0,0 +1,27 @@
+import './notification.css';
+
+export type NotificationProps = {
+  type: 'info' | 'success' | 'error';
+} & React.HTMLAttributes<HTMLDivElement>;
+
+export function Notification({
+  type,
+  className = '',
+  children,
+  ...props
+}: NotificationProps) {
+  return (
+    <div
+      className={`container notification--${type} full-width ${className}`}
+      {...props}
+    >
+      {children || (
+        <p>
+          {type === 'success'
+            ? 'Operation was successful'
+            : 'An error occurred'}
+        </p>
+      )}
+    </div>
+  );
+}
