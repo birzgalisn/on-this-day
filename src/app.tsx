09e4f01994e0e619776d2aae40683f38b0dc6ab6
Niks <hello@niksbirzgalis.com>
2025-03-29 19:04:14 +0000

    feat: use added on this day feature

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { OnThisDay } from './features/on-this-day';
+import './app.css';
+
+function App() {
+  return (
+    <main className="main">
+      <OnThisDay />
+    </main>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store.ts';
-import App from './App.tsx';
+import App from './app.tsx';
 import './reset.css';
 import './main.css';
 
