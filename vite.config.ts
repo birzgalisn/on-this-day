9d24e43fa1bf6da6d0d8fc51e68922746d32964e
Niks <hello@niksbirzgalis.com>
2025-03-30 10:55:43 +0000

    feat: setup tests configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    css: true,
+    setupFiles: 'src/__tests__/setup.ts',
+    coverage: {
+      provider: 'v8',
+    },
+  },
 });
