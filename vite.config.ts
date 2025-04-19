ddb31465afe304bded943b9a46d602d1d71b1c1b
Niks <hello@niksbirzgalis.com>
2025-04-19 21:00:59 +0000

    feat: add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://github.com/facebook/react/blob/main/compiler/packages/babel-plugin-react-compiler/src/Entrypoint/Options.ts#L41
+const ReactCompilerConfig = {};
+
+// https://vite.dev/config
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
   test: {
     environment: 'jsdom',
     globals: true,
     css: true,
     setupFiles: 'src/__tests__/setup.ts',
-    coverage: {
-      provider: 'v8',
-    },
+    coverage: { provider: 'v8' },
   },
 });
